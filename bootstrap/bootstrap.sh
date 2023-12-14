1259a9ccfd61134607638b52bc50e89ac9310df9
Daniel Alder <daald@users.noreply.github.com>
2023-12-14 22:11:52 +0100

    Cleanup and improve startup behaviour

--- bootstrap/bootstrap.sh
+++ bootstrap/bootstrap.sh
@@ -4,11 +4,13 @@ set -e
 
 /opt/bootstrap/genconfig.sh
 
-cat /etc/passwd
+set +e
+
 chown munin /var/lib/munin/
 
+mkdir -p /var/lib/munin/cgi-tmp/
 chown httpd -R /var/lib/munin/cgi-tmp/
 
 touch /var/log/munin/munin-graph.log
-chown httpd.munin /var/log/munin/munin-graph.log
-chmod ug+rw /var/log/munin/munin-graph.log
+chown httpd.munin /var/log/munin/munin-graph.log /var/lib/munin/limits
+chmod ug+rw /var/log/munin/munin-graph.log /var/lib/munin/limits
