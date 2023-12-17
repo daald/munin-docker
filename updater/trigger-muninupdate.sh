4157077700a4c3944545bed5138bf8bd976d613a
Daniel Alder <daald@users.noreply.github.com>
2023-12-17 20:20:16 +0100

    Add whitelist support for munin-update alerts
    whitelist in this case means that the whitelistes expressions don't trigger an alarm

--- updater/trigger-muninupdate.sh
+++ updater/trigger-muninupdate.sh
@@ -6,7 +6,16 @@ lastlogpos="$(stat -c%s "$logfile" 2>/dev/null || echo 0)"
 /usr/bin/munin-cron
 
 if [ -n "$SMTPHOST" ]; then
-  tail -c+"$lastlogpos" "$logfile" | grep -E '\[(WARNING|ERROR|FATAL)\]' >/tmp/muninupdate-lastcall
+  if [ -s /etc/munin/update-error-whitelist.regex ]; then
+    whitelist() {
+      grep -vEf /etc/munin/update-error-whitelist.regex
+    }
+  else
+    whitelist() {
+      cat
+    }
+  fi
+  tail -c+"$lastlogpos" "$logfile" | grep -E '\[(WARNING|ERROR|FATAL)\]' >/tmp/muninupdate-lastcall | whitelist
   if [ -s /tmp/muninupdate-lastcall ]; then
     cat /tmp/muninupdate-lastcall | sendmail "$SMTPTO"
   fi
